36ed4aff8f0e9d0441d01958aa01bebfd9c7c94d
ryskit <ryosukekida.71@gmail.com>
2023-01-21 02:23:43 +0900

    1.3 Lexerの実装done

--- token/token.go
+++ token/token.go
@@ -22,9 +22,21 @@ const (
 	RBRACE = "}"
 
 	// キーワード
-	FUNCTION = "function"
+	FUNCTION = "fn"
 	LET      = "let"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
 
 type TokenType string
 
