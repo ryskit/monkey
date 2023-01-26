d6904be569d9f060a398bfef7f0fb59a6c3a7bc3
ryskit <ryosukekida.71@gmail.com>
2023-01-27 01:23:48 +0900

    done 束縛と環境

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/ryskit/monkey/evaluator"
 	"github.com/ryskit/monkey/lexer"
+	"github.com/ryskit/monkey/object"
 	"github.com/ryskit/monkey/parser"
 	"io"
 )
@@ -13,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
